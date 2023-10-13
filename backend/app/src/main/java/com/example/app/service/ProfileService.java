222521d4c50c4b2431ff6241e6f4513fd4335f91
ryu-k67 <o2ro8chi@gmail.com>
2023-10-14 01:28:53 +0900

    プロフィール更新

--- backend/app/src/main/java/com/example/app/service/ProfileService.java
+++ backend/app/src/main/java/com/example/app/service/ProfileService.java
@@ -1,7 +1,11 @@
 package com.example.app.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
 import com.example.app.models.Profile;
 import com.example.app.models.ProfileRequest;
@@ -27,12 +31,49 @@ public class ProfileService {
         profile.setUsername(username);
         profile.setUserid(userid);
         return profileRepository.save(profile)
-        .map(savedProfile -> savedProfile.getUserid())
+        .map(savedProfile -> savedProfile.getProfileid())
         .onErrorReturn(-1);
     }
 
     // プロフィールの取得
     public Mono<Profile> getProfile(int userid){
         return profileRepository.findById(userid);
+    }
+
+    public Mono<Integer> updateProfile(ProfileRequest profileRequest){
+        // var profile = new Profile();
+        String username = profileRequest.getUsername();
+        //byte[] profImg = profileRequest.getProf_img();
+        int userid = profileRequest.getUserid();
+
+        // profile.setProfileid(2);
+        // profile.setFollowernumber(0);
+        // profile.setFollowingnumber(0);
+        // profile.setPostnumber(0);
+        // profile.setProfimg(null);
+        // profile.setUsername(username);
+        // profile.setUserid(userid);
+        // return profileRepository.save(profile)
+        // .map(savedProfile -> savedProfile.getUserid())
+        // .onErrorReturn(-1);
+
+        // return profileRepository.findByUserid(userid).map(Optional::of).defaultIfEmpty(Optional.empty())
+        // .flatMap(optionalProfile -> {
+        //     if (optionalProfile.isPresent()) {
+        //         profile.setProfileid(2);
+        //         return ProfileRepository.save(profile);
+        //     }
+
+        //     return Mono.empty();
+        // });
+
+        return profileRepository.findByUserid(userid)
+            .map(c -> new Profile(c.getProfileid(),userid,username,c.getProfimg(),c.getFollowingnumber(),c.getFollowernumber(),c.getPostnumber()))
+            .flatMap(profileRepository::save)
+            .map(savedProfile -> savedProfile.getProfileid())
+            .onErrorReturn(-1);
+            // .flatMap(profile ->
+            //     ServerResponse.status(HttpStatus.CREATED));
+                    // .body(profile.map(c -> new CustomerDto(c.getId(), c.getUserName())), CustomerDto.class));
     }
 }
